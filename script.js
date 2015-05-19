fe239ab7ff4c5a15947690c871ec99677600e329
Heather Booker <heather.booker@hotmail.com>
2015-05-19 00:59:24 -0700

    can add new animals to array, image will appear, but no update to list

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 var animals = [{
-    name: 'lion',
+    name: 'Lion',
     image: 'http://i1-news.softpedia-static.com/images/news2/Picture-of-the-Day-Real-Life-Simba-and-Mufasa-Caught-on-Camera-in-Tanzania-392687-2.jpg',
     ferocity: 7,
     description: 'Large Carnivorous Feline',
     type: 'Mammal'
-}]
-
+}, {
+    name: 'Penguin',
+    image: 'http://www.therealdavidlevin.com/wp-content/uploads/2012/06/penguinCloseup750_231715.jpg',
+    ferocity: 4,
+    description: "Fancy Lookin' Killer Whale Snacks",
+    type: 'Bird'
+}];
+console.log('i work')
 $(document).ready(function() {
 
     $("#addImage").click(function() {
@@ -17,21 +23,37 @@ $(document).ready(function() {
         var imgURL = $('#hiddenURL').val();
         var ferocity = $('#ferocity').val();
         var descriptn = $('#description').val();
-        var animalCategory = $('input:radio[name="user[role]"]:checked').val();
-        if (animalCategory === 'on') {
-            var animalType = $('#otherRadioText').val();
+        var animalType = $('input:radio[name="user[role]"]:checked').val();
+        if (animalType === 'on') {
+            animalType = $('#otherRadioText').val();
         }
-        console.log(animalName, imgURL, ferocity, descriptn, animalType)
+        var newAnimal = $(createAnimalObject(animalName, imgURL, ferocity, descriptn, animalType));
+        console.log(animals[2]);
+        $(putAnimalInZoo(2));
     });
 
 });
 
-function createAnimalObject(title, name, pic, feroc, descript, type) {
-    var title = {
+function createAnimalObject(name, pic, feroc, descript, type) {
+
+    var anonAnimal = {
         name: name,
+        image: pic,
+        ferocity: feroc,
+        description: descript,
+        type: type
     }
+
+    animals.push(anonAnimal);
+    return animals;
 }
 
 function publishAnimal() {
 
+}
+
+function putAnimalInZoo(animalPositionInArray) {
+    var newPic = animals[animalPositionInArray].image;
+    $("#zoo").append('<img id="animal' + animalPositionInArray + '" class="animal" src="' + newPic + '">');
+    // document.getElementById('#zoo').innerHTML += '<img src="' + newPic + '">'
 }
